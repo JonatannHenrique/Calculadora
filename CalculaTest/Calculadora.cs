7f7f3bd1fa653e5ca9eccc0f9356df117baee5af
Jonatan <luana.dabella2006@gmail.com>
2025-05-26 11:31:15 -0300

    Adicionar arquivos de projeto.

---
+++ CalculaTest/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaTest
+{
+    internal class Calculadora
+    {
+        public static double calcular(double a, double b, string operacao)
+        {
+            if (operacao == "+") return a + b;
+            if (operacao == "-") return a + b;
+            if (operacao == "*") return a + b;
+            if (operacao == "/")
+
+                return a / b;
+
+            return double.NaN;
+        }
+    }
+}
---
+++ CalculaTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("↓ CALCULADORA ↓");
+                Console.Write("Digite o primeiro numero: ");
+                string n1 = Console.ReadLine();
+                Console.WriteLine("Escolha (+ <Mas , - <menos, / < divisão , * < multiplicaçao )");
+                string op = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Digite o Segundo Numero: ");
+                string n2 = Console.ReadLine();
+
+                if (double.TryParse(n1, out double num1) && double.TryParse(n2, out double num2))
+                {
+                    double resultado = Calculadora.calcular(num1, num2, op);
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
